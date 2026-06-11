6e839c2f83298f5eb2a2e592fc16d9811f76c5cc
MrShadow335 <shadow335@crowdfaq.com>
2026-06-11 17:56:19 +0530

    Add start.bat and update backend files

---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd C:\Users\HP\OneDrive\Desktop\p\Crowd-Sourced-FAQ-Mangement\backend && npm run dev"
+start cmd /k "cd C:\Users\HP\OneDrive\Desktop\p\Crowd-Sourced-FAQ-Mangement\frontend && npm run dev"
+start cmd /k "cd C:\Users\HP\OneDrive\Desktop\p\Crowd-Sourced-FAQ-Mangement\nlp-service && python app.py"
